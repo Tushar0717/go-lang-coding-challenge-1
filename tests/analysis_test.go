54a0f81cda4fa9ceccb8e844e596b27145316da4
Pash O'Connor <ix.pash@gmail.com>
2024-06-11 12:18:40 -0400

    Coding challenge

---
+++ tests/analysis_test.go
@@ -0,0 +1,22 @@
+package tests
+
+import (
+	"testing"
+	"net/http"
+	"net/http/httptest"
+	"github.com/stretchr/testify/assert"
+	"github.com/path/to/your/package/models"
+	"github.com/path/to/your/package/controllers"
+)
+
+func TestOptionsContractModelValidation(t *testing.T) {
+	// Your code here
+}
+
+func TestAnalysisEndpoint(t *testing.T) {
+	// Your code here
+}
+
+func TestIntegration(t *testing.T) {
+	// Your code here
+}
